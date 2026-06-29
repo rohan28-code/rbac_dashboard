6e82675e3ef920664c4609f9f02ae326035e7361
Rohan <ryadav19802@gmail.com>
2026-06-29 16:55:31 +0530

    fix hardcoded secret key

--- rbac_dashboard/settings.py
+++ rbac_dashboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+z9mlz^kmbqe+o+0g!!n_fi-v81x!r@ak*q1%^))#@&e(-m(t'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-dev-key-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
